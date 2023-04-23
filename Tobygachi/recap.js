06a87f8d6f76ad63861bdb83f5f570e7df3c8436
0AYU0 <awy001@ucsd.edu>
2023-04-22 19:28:09 -0700

    Add recap information

--- Tobygachi/recap.js
+++ Tobygachi/recap.js
@@ -2,14 +2,28 @@ import React, { useState, useEffect } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 const Recap = ({ navigation, route }) => {
   console.log(route);
-  return <Text style={styles.text}>End Recap</Text>;
+  const {
+    suddenStops,
+    suddenAccelerations,
+    suddenTurnRight,
+    suddenTurnLeft,
+    goodStops,
+    goodAccelerations,
+  } = route.params.recapStats;
+  return (
+    <View>
+      <Text style={styles.text}>{goodAccelerations} good starts</Text>
+      <Text style={styles.text}>{goodStops} good stops</Text>
+      <Text style={styles.text}>{suddenAccelerations} sudden starts</Text>
+      <Text style={styles.text}>{suddenStops} suddent stops</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontFamily: 'Baloo2',
+    fontFamily: "Baloo2",
   },
 });
-
 
 export default Recap;
